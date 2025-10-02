b5d5f988f752eb120d3ad96b8b5bb3b977395cb4
Buddarraju Lalit <163729989+buddarrajulalit@users.noreply.github.com>
2025-10-02 12:32:40 +0530

    Initial commit: SafeChild Alert System with FastAPI

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from routes import text_routes, image_routes, audio_routes, sse_routes
+<<<<<<< HEAD
+app = FastAPI(title='HappyChild - AI Guardian')
+app.include_router(text_routes.router, prefix='/analyze/text')
+app.include_router(image_routes.router, prefix='/analyze/image')
+app.include_router(audio_routes.router, prefix='/analyze/audio')
+app.include_router(sse_routes.router, prefix='/alerts')
+@app.get('/')
+def root():
+    return {'message': 'HappyChild API running!'}
+=======
 
 app = FastAPI(title="HappyChild - AI Guardian")
 
@@ -12,3 +22,4 @@ app.include_router(sse_routes.router, prefix="/alerts")
 @app.get("/")
 def root():
     return {"message": "HappyChild API running!"}
+>>>>>>> 32fca09c8497922d576e4ba37eef8bbdd9c9ed46
